9ef4b167b06cf596a0f9c9c292a1526e860ea07c
Gino Latorilla <pblatorilla+github@gmail.com>
2024-06-13 23:54:27 +0800

    Identify unused configmaps for free pods and deployments

--- src/k8s.rs
+++ src/k8s.rs
@@ -1,5 +1,8 @@
+use std::borrow::Cow;
+
 use clap::ValueEnum;
-use k8s_openapi::api::core::v1::{ConfigMap, Pod};
+use k8s_openapi::api::apps::v1::Deployment;
+use k8s_openapi::api::core::v1::{ConfigMap, Pod, PodSpec};
 use k8s_openapi::{Metadata, NamespaceResourceScope, Resource};
 use kube::api::ObjectMeta;
 use kube::{
@@ -31,17 +34,36 @@ async fn clean_config_maps(
     client: Client,
     namespace: Option<&String>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let config_maps: Vec<String> = get_api::<ConfigMap>(client.clone(), namespace)
+    let config_maps: Vec<String> = get_api::<ConfigMap>(Cow::Borrowed(&client), namespace)
         .list(&ListParams::default())
         .await?
         .into_iter()
         .map(|cm| cm.name_any())
         .collect();
 
-    let used_config_maps: Vec<String> = free_pods(client, namespace)
+    let used_config_maps: Vec<String> = free_pods(&client, namespace)
         .await?
         .into_iter()
-        .flat_map(|pod| extract_config_maps_from_pod(&pod))
+        .filter(|pod| pod.spec.is_some())
+        .flat_map(|pod| extract_config_maps_from(&pod, &pod.to_owned().spec.unwrap()))
+        .chain(
+            get_api::<Deployment>(Cow::Borrowed(&client), namespace)
+                .list(&ListParams::default())
+                .await?
+                .into_iter()
+                .filter(|deploy| {
+                    deploy
+                        .spec
+                        .as_ref()
+                        .map_or(false, |spec| spec.template.spec.is_some())
+                })
+                .flat_map(|deploy| {
+                    extract_config_maps_from(
+                        &deploy,
+                        &deploy.spec.clone().unwrap().template.spec.unwrap(),
+                    )
+                }),
+        )
         .collect();
 
     config_maps
@@ -51,16 +73,26 @@ async fn clean_config_maps(
     Ok(())
 }
 
+fn get_api<R: Resource<Scope = NamespaceResourceScope> + Metadata<Ty = ObjectMeta>>(
+    client: Cow<Client>,
+    namespace: Option<&String>,
+) -> Api<R> {
+    match namespace {
+        Some(n) => Api::namespaced(client.into_owned(), n),
+        None => Api::default_namespaced(client.into_owned()),
+    }
+}
+
 async fn free_pods(
-    client: Client,
+    client: &Client,
     namespace: Option<&String>,
 ) -> Result<Vec<Pod>, Box<dyn std::error::Error>> {
-    let pods: Api<Pod> = get_api(client, namespace);
+    let pods: Api<Pod> = get_api(Cow::Borrowed(client), namespace);
     let static_pods = pods
         .list(&ListParams::default())
         .await?
         .into_iter()
-        .filter(|pod| !pod.owner_references().is_empty())
+        .filter(|pod| pod.owner_references().is_empty())
         .inspect(|pod| {
             debug!(
                 "Ownerless pod {} found in namespace {}.",
@@ -72,70 +104,126 @@ async fn free_pods(
     Ok(static_pods)
 }
 
-fn extract_config_maps_from_pod(pod: &Pod) -> Vec<String> {
-    let mut a = extract_config_maps_from_pod_volumes(pod);
-    let mut b = extract_config_maps_from_projected_volumes(pod);
-    a.append(&mut b);
-    a
+fn extract_config_maps_from<
+    R: Resource<Scope = NamespaceResourceScope> + Metadata<Ty = ObjectMeta>,
+>(
+    resource: &R,
+    pod_spec: &PodSpec,
+) -> Vec<String> {
+    extract_config_maps_from_env_vars(resource, Cow::Borrowed(pod_spec))
+        .into_iter()
+        .chain(extract_config_maps_from_env_from(
+            resource,
+            Cow::Borrowed(pod_spec),
+        ))
+        .chain(extract_config_maps_from_volumes(
+            resource,
+            Cow::Borrowed(pod_spec),
+        ))
+        .chain(extract_config_maps_from_projected_volumes(
+            resource,
+            Cow::Borrowed(pod_spec),
+        ))
+        .collect()
 }
 
-fn extract_config_maps_from_pod_volumes(pod: &Pod) -> Vec<String> {
-    pod.spec
-        .as_ref()
-        .and_then(|spec| spec.volumes.as_ref())
-        .map_or(Vec::new(), |volumes| {
-            volumes
-                .iter()
-                .filter_map(|volume| Some((&volume.name, volume.config_map.as_ref()?)))
-                .filter_map(|(v, cvs)| Some((v, cvs.name.as_ref()?)))
-                .inspect(|(v, cm)| {
-                    debug!(
-                        "Reference to config map {} found in volume {} in pod {} in namespace {}.",
-                        cm,
-                        v,
-                        pod.name_any(),
-                        pod.namespace().unwrap_or_default()
-                    )
-                })
-                .map(|(_, cm)| cm.clone())
-                .collect()
+fn extract_config_maps_from_volumes<
+    R: Resource<Scope = NamespaceResourceScope> + Metadata<Ty = ObjectMeta>,
+>(
+    owner: &R,
+    pod_spec: Cow<PodSpec>,
+) -> Vec<String> {
+    pod_spec.into_owned().volumes.map_or(Vec::new(), |volumes| {
+        volumes
+            .into_iter()
+            .filter_map(|volume| Some((volume.name, volume.config_map?.name?)))
+            .inspect(|(volume, config_map)| {
+                debug!(
+                    "Reference to config map {config_map} found in volume {volume} in {} {} in namespace {}.",
+                    R::KIND,
+                    owner.name_any(),
+                    owner.namespace().unwrap_or_default()
+                )
+            })
+            .map(|(_, config_map)| config_map)
+            .collect()
+    })
+}
+
+fn extract_config_maps_from_projected_volumes<
+    R: Resource<Scope = NamespaceResourceScope> + Metadata<Ty = ObjectMeta>,
+>(
+    owner: &R,
+    pod_spec: Cow<PodSpec>,
+) -> Vec<String> {
+    pod_spec.into_owned().volumes.map_or(Vec::new(), |volumes| {
+        volumes
+            .into_iter()
+            .filter_map(|volume| Some((volume.name, volume.projected?.sources?)))
+            .flat_map(|(volume, projections)| {
+                projections
+                    .into_iter()
+                    .filter_map(move |projection| Some((volume.to_owned(), projection.config_map?.name?)))
+            })
+            .inspect(|(volume, config_map)| {
+                debug!(
+                    "Reference to config map {config_map} found in a projected volume {volume} in {} {} in namespace {}.",
+                    R::KIND,
+                    owner.name_any(),
+                    owner.namespace().unwrap_or_default()
+                )
+            })
+            .map(|(_, config_map)| config_map)
+            .collect()
+    })
+}
+
+fn extract_config_maps_from_env_vars<
+    R: Resource<Scope = NamespaceResourceScope> + Metadata<Ty = ObjectMeta>,
+>(
+    owner: &R,
+    pod_spec: Cow<PodSpec>,
+) -> Vec<String> {
+    pod_spec
+        .into_owned()
+        .containers
+        .into_iter()
+        .filter_map(|container| Some((container.name, container.env?)))
+        .flat_map(|(container, env_vars)| env_vars.into_iter().map(move |env_var| (container.to_owned(), env_var)))
+        .filter_map(|(container, env_var)| Some((container, env_var.name, env_var.value_from?.config_map_key_ref?.name?)))
+        .inspect(|(container, env_var, config_map)|{
+            debug!(
+                "Reference to config_map {config_map} found in the env var {env_var} of container {container} in the pod spec of {} {} in namespace {}.",
+                R::KIND,
+                owner.name_any(),
+                owner.namespace().unwrap_or_default()
+            )
         })
+        .map(|(_, _, config_map)| config_map)
+        .collect()
 }
 
-fn extract_config_maps_from_projected_volumes(pod: &Pod) -> Vec<String> {
-    pod.spec
-        .as_ref()
-        .and_then(|spec| spec.volumes.as_ref())
-        .map_or(Vec::new(), |volumes| {
-            volumes
-                .iter()
-                .filter_map(|volume| Some((&volume.name, volume.projected.as_ref()?)))
-                .filter_map(|(v, pvs)| Some((v, pvs.sources.as_ref()?)))
-                .flat_map(|(v, vps)| {
-                    vps.iter()
-                        .filter_map(move |vp| Some((v, vp.config_map.as_ref()?)))
-                })
-                .filter_map(|(v, cmp)| Some((v, cmp.name.as_ref()?)))
-                .inspect(|(v, cm)| {
-                    debug!(
-                        "Reference to config map {} found in volume {} in pod {} in namespace {}.",
-                        cm,
-                        v,
-                        pod.name_any(),
-                        pod.namespace().unwrap_or_default()
-                    )
-                })
-                .map(|(_, cm)| cm.clone())
-                .collect()
+fn extract_config_maps_from_env_from<
+    R: Resource<Scope = NamespaceResourceScope> + Metadata<Ty = ObjectMeta>,
+>(
+    owner: &R,
+    pod_spec: Cow<PodSpec>,
+) -> Vec<String> {
+    pod_spec
+        .into_owned()
+        .containers
+        .into_iter()
+        .filter_map(|container| Some((container.name, container.env_from?)))
+        .flat_map(|(container, sources)| sources.into_iter().map(move |source| (container.to_owned(), source)))
+        .filter_map(|(container, source)| Some((container, source.config_map_ref?.name?)))
+        .inspect(|(container, config_map)|{
+            debug!(
+                "Reference to config_map {config_map} found in the envFrom of container {container} in the pod spec of {} {} in namespace {}.",
+                R::KIND,
+                owner.name_any(),
+                owner.namespace().unwrap_or_default()
+            )
         })
-}
-
-fn get_api<R: Resource<Scope = NamespaceResourceScope> + Metadata<Ty = ObjectMeta>>(
-    client: Client,
-    namespace: Option<&String>,
-) -> Api<R> {
-    match namespace {
-        Some(n) => Api::namespaced(client, n),
-        None => Api::default_namespaced(client),
-    }
+        .map(|(_, config_map)| config_map)
+        .collect()
 }
