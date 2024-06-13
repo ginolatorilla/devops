c9802cc33ff2b6dcc3e0cb361b9d1587234e4511
Gino Latorilla <pblatorilla+github@gmail.com>
2024-06-13 21:06:15 +0800

    Identify configmaps not used by free pods in their volumes

--- src/k8s.rs
+++ src/k8s.rs
@@ -6,7 +6,7 @@ use kube::{
     api::{Api, ListParams, ResourceExt},
     Client,
 };
-use log::debug;
+use log::{debug, info};
 
 #[derive(ValueEnum, Debug, Clone)]
 pub enum Resources {
@@ -41,13 +41,13 @@ async fn clean_config_maps(
     let used_config_maps: Vec<String> = free_pods(client, namespace)
         .await?
         .into_iter()
-        .flat_map(|pod| extract_config_maps_from_pod_volumes(&pod))
+        .flat_map(|pod| extract_config_maps_from_pod(&pod))
         .collect();
 
-    let _ = config_maps
+    config_maps
         .iter()
         .filter(|cm_name| !used_config_maps.contains(&cm_name))
-        .for_each(|cm_name| println!("Found unused configmap: {}.", cm_name));
+        .for_each(|cm_name| info!("Found unused configmap: {}.", cm_name));
     Ok(())
 }
 
@@ -60,48 +60,74 @@ async fn free_pods(
         .list(&ListParams::default())
         .await?
         .into_iter()
-        .filter(|p| -> bool {
-            let is_orphan = p.metadata().owner_references.is_none();
-            if is_orphan {
-                debug!(
-                    "Ownerless pod {} found in namespace {}.",
-                    p.name_any(),
-                    p.namespace().unwrap_or_default(),
-                );
-            }
-            is_orphan
+        .filter(|pod| !pod.owner_references().is_empty())
+        .inspect(|pod| {
+            debug!(
+                "Ownerless pod {} found in namespace {}.",
+                pod.name_any(),
+                pod.namespace().unwrap_or_default(),
+            );
         })
         .collect();
     Ok(static_pods)
 }
 
+fn extract_config_maps_from_pod(pod: &Pod) -> Vec<String> {
+    let mut a = extract_config_maps_from_pod_volumes(pod);
+    let mut b = extract_config_maps_from_projected_volumes(pod);
+    a.append(&mut b);
+    a
+}
+
 fn extract_config_maps_from_pod_volumes(pod: &Pod) -> Vec<String> {
-    pod.spec.as_ref().map_or(Vec::new(), |pod_spec| {
-        pod_spec.volumes.as_ref().map_or(Vec::new(), |volumes| {
+    pod.spec
+        .as_ref()
+        .and_then(|spec| spec.volumes.as_ref())
+        .map_or(Vec::new(), |volumes| {
             volumes
                 .iter()
-                .filter_map(|volume| {
-                    volume
-                        .config_map
-                        .as_ref()
-                        .and_then(|volume_source| {
-                            let config_map_name = volume_source.name.as_ref();
-                            debug!(
-                                "Reference to config map {:?} found in volume {} of pod {} in namespace {}.",
-                                config_map_name,
-                                volume.name,
-                                pod.name_any(),
-                                pod.namespace().unwrap_or_default()
-                            );
-                            config_map_name
-                        })
+                .filter_map(|volume| Some((&volume.name, volume.config_map.as_ref()?)))
+                .filter_map(|(v, cvs)| Some((v, cvs.name.as_ref()?)))
+                .inspect(|(v, cm)| {
+                    debug!(
+                        "Reference to config map {} found in volume {} in pod {} in namespace {}.",
+                        cm,
+                        v,
+                        pod.name_any(),
+                        pod.namespace().unwrap_or_default()
+                    )
                 })
-                .fold(Vec::new(), |mut config_maps, config_map_name| {
-                    config_maps.push(config_map_name.clone());
-                    config_maps
-                })
+                .map(|(_, cm)| cm.clone())
+                .collect()
         })
-    })
+}
+
+fn extract_config_maps_from_projected_volumes(pod: &Pod) -> Vec<String> {
+    pod.spec
+        .as_ref()
+        .and_then(|spec| spec.volumes.as_ref())
+        .map_or(Vec::new(), |volumes| {
+            volumes
+                .iter()
+                .filter_map(|volume| Some((&volume.name, volume.projected.as_ref()?)))
+                .filter_map(|(v, pvs)| Some((v, pvs.sources.as_ref()?)))
+                .flat_map(|(v, vps)| {
+                    vps.iter()
+                        .filter_map(move |vp| Some((v, vp.config_map.as_ref()?)))
+                })
+                .filter_map(|(v, cmp)| Some((v, cmp.name.as_ref()?)))
+                .inspect(|(v, cm)| {
+                    debug!(
+                        "Reference to config map {} found in volume {} in pod {} in namespace {}.",
+                        cm,
+                        v,
+                        pod.name_any(),
+                        pod.namespace().unwrap_or_default()
+                    )
+                })
+                .map(|(_, cm)| cm.clone())
+                .collect()
+        })
 }
 
 fn get_api<R: Resource<Scope = NamespaceResourceScope> + Metadata<Ty = ObjectMeta>>(
