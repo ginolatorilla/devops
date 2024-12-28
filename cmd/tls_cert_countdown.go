a926075e577480cf86999137bbcd479da79a2a29
Gino Latorilla <pblatorilla+github@gmail.com>
2024-12-28 15:28:04 +0800

    tls-cert-countdown should print a shorter version

--- cmd/tls_cert_countdown.go
+++ cmd/tls_cert_countdown.go
@@ -13,6 +13,7 @@ import (
 const _iso8601 = "2006-01-02 15:04:05Z"
 
 func newTlsCertCountdownCmd() *cobra.Command {
+	var short bool
 	cmd := &cobra.Command{
 		Use:   "tls-cert-countdown",
 		Short: "Check the expiration date of TLS certificates",
@@ -24,23 +25,18 @@ For example:
 			AppName,
 		),
 		Run: func(cmd *cobra.Command, args []string) {
-			checkTlsCertCountdown(cmd.InOrStdin())
+			checkTlsCertCountdown(cmd.InOrStdin(), cmd.OutOrStdout(), short)
 		},
 	}
+	cmd.Flags().BoolVar(&short, "short", false, "Print a short version of the output")
 	return cmd
 }
 
-func checkTlsCertCountdown(stdin io.Reader) {
+func checkTlsCertCountdown(stdin io.Reader, stdout io.Writer, short bool) {
 	input := string(_must(io.ReadAll(stdin)))
 	for _, line := range strings.Split(input, "\n") {
 		if strings.Contains(line, "notAfter=") {
-			expDate := parseOpenSslDateLine(line)
-			friendlyTime := humanize.Time(expDate)
-			if strings.Contains(friendlyTime, "ago") {
-				fmt.Printf("The certificate expired %s\n", friendlyTime)
-			} else {
-				fmt.Printf("The certificate will expire in %s\n", friendlyTime)
-			}
+			printReport(stdout, parseOpenSslDateLine(line), short)
 			return
 		}
 	}
@@ -51,4 +47,18 @@ func checkTlsCertCountdown(stdin io.Reader) {
 func parseOpenSslDateLine(line string) time.Time {
 	parts := strings.Split(line, "=")
 	return _must(time.Parse(_iso8601, parts[1]))
+}
+
+func printReport(out io.Writer, expDate time.Time, short bool) {
+	friendlyTime := humanize.Time(expDate)
+	if short {
+		fmt.Fprintln(out, friendlyTime)
+		return
+	}
+
+	if strings.Contains(friendlyTime, "ago") {
+		fmt.Fprintf(out, "The certificate expired %s\n", friendlyTime)
+	} else {
+		fmt.Fprintf(out, "The certificate will expire in %s\n", friendlyTime)
+	}
 }
