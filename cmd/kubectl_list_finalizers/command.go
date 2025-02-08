8c0cd5fc33da3fa37c8e53162c8909ad5fc4a0bb
Gino Latorilla <pblatorilla+github@gmail.com>
2025-02-08 15:38:13 +0800

    Refactor list-finalizers

--- cmd/kubectl_list_finalizers/command.go
+++ cmd/kubectl_list_finalizers/command.go
@@ -1,6 +1,7 @@
 package kubectl_list_finalizers
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -14,6 +15,7 @@ import (
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
 )
 
@@ -27,26 +29,9 @@ func NewCommand(apiFactory kube.DynamicApiFactory) *cobra.Command {
 }
 
 func listResourceUsers(a kube.HandlerArgs) (metaV1.Table, error) {
-	_, resources, err := a.DiscoveryApi.ServerGroupsAndResources()
-	for _, resource := range resources {
-		var newApiResources []metaV1.APIResource
-		for _, apiResource := range resource.APIResources {
-			if strings.Contains(apiResource.Name, "/") {
-				continue
-			}
-			if !slices.Contains(apiResource.Verbs, "list") && !slices.Contains(apiResource.Verbs, "get") {
-				continue
-			}
-			newApiResources = append(newApiResources, apiResource)
-		}
-		resource.APIResources = newApiResources
-	}
+	gvrs, err := getAllGroupVersionResources(a)
 	if err != nil {
-		return metaV1.Table{}, fmt.Errorf("failed to get server resources: %w", err)
-	}
-	gvrs, err := discovery.GroupVersionResources(resources)
-	if err != nil {
-		return metaV1.Table{}, fmt.Errorf("failed to convert server API resource lists to group version resources: %w", err)
+		return metaV1.Table{}, fmt.Errorf("failed to get all group version resources: %w", err)
 	}
 
 	jq, err := gojq.Parse(".items[] | {kind: .kind, namespace: .metadata.namespace, name: .metadata.name, finalizers: .metadata.finalizers}")
@@ -62,7 +47,7 @@ func listResourceUsers(a kube.HandlerArgs) (metaV1.Table, error) {
 		},
 	}
 
-	for gvr := range gvrs {
+	for _, gvr := range gvrs {
 		resources, err := a.DynamicApi.Resource(gvr).Namespace(a.Namespace).List(a.Cmd.Context(), metaV1.ListOptions{})
 		if err != nil {
 			if !errors.Is(err, fmt.Errorf("the server could not find the requested resource")) {
@@ -71,49 +56,89 @@ func listResourceUsers(a kube.HandlerArgs) (metaV1.Table, error) {
 			continue
 		}
 
-		rawJson, _ := resources.MarshalJSON()
-		var resourcesAsJson map[string]any
-		json.Unmarshal(rawJson, &resourcesAsJson)
-
-		iter := jq.RunWithContext(a.Cmd.Context(), resourcesAsJson)
-		for {
-			untyped, ok := iter.Next()
-			if !ok {
-				break
-			}
-
-			rawJson, err := json.Marshal(untyped)
-			if err != nil {
-				panic(fmt.Errorf("failed to marshal untyped object to JSON: %w", err))
-			}
-			var object struct {
-				Kind       string   `json:"kind"`
-				Name       string   `json:"name"`
-				Namespace  string   `json:"namespace"`
-				Finalizers []string `json:"finalizers"`
-			}
-			err = json.Unmarshal(rawJson, &object)
-			if err != nil {
-				panic(err)
-			}
-
-			for _, finalizer := range object.Finalizers {
-				table.Rows = append(table.Rows, metaV1.TableRow{
-					Cells: []interface{}{
-						object.Kind,
-						object.Name,
-						finalizer,
-					},
-					Object: runtime.RawExtension{Object: &unstructured.Unstructured{
-						Object: map[string]interface{}{
-							"metadata": map[string]interface{}{
-								"namespace": object.Namespace,
-							},
-						},
-					}},
-				})
-			}
-		}
+		queryDynamicResource(a.Cmd.Context(), jq, resources, func(rawJson []byte) {
+			var object objectWithFinalizers
+			json.Unmarshal(rawJson, &object)
+			recordObjectsWithFinalizers(&table, object)
+		})
 	}
 	return table, nil
+}
+
+func getAllGroupVersionResources(a kube.HandlerArgs) ([]schema.GroupVersionResource, error) {
+	_, resources, err := a.DiscoveryApi.ServerGroupsAndResources()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get server resources: %w", err)
+	}
+	for _, resource := range resources {
+		var filteredApiResources []metaV1.APIResource
+		for _, apiResource := range resource.APIResources {
+			if strings.Contains(apiResource.Name, "/") {
+				continue
+			}
+			if !slices.Contains(apiResource.Verbs, "list") && !slices.Contains(apiResource.Verbs, "get") {
+				continue
+			}
+			filteredApiResources = append(filteredApiResources, apiResource)
+		}
+		resource.APIResources = filteredApiResources
+	}
+	gvrs, err := discovery.GroupVersionResources(resources)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert server API resource lists to group version resources: %w", err)
+	}
+	gvrSlice := make([]schema.GroupVersionResource, len(gvrs))
+	for gvr := range gvrs {
+		gvrSlice = append(gvrSlice, gvr)
+	}
+	return gvrSlice, nil
+}
+
+type jsonSerializable interface {
+	MarshalJSON() ([]byte, error)
+}
+
+func queryDynamicResource(ctx context.Context, jq *gojq.Query, object jsonSerializable, handler func(raw []byte)) {
+	rawJson, _ := object.MarshalJSON()
+	var jsonObject map[string]any
+	json.Unmarshal(rawJson, &jsonObject)
+
+	iter := jq.RunWithContext(ctx, jsonObject)
+	for {
+		typeErased, ok := iter.Next()
+		if !ok {
+			break
+		}
+		rawJson, err := json.Marshal(typeErased)
+		if err != nil {
+			panic(fmt.Errorf("failed to marshal untyped object to JSON: %w", err))
+		}
+		handler(rawJson)
+	}
+}
+
+type objectWithFinalizers struct {
+	Kind       string   `json:"kind"`
+	Name       string   `json:"name"`
+	Namespace  string   `json:"namespace"`
+	Finalizers []string `json:"finalizers"`
+}
+
+func recordObjectsWithFinalizers(table *metaV1.Table, object objectWithFinalizers) {
+	for _, finalizer := range object.Finalizers {
+		table.Rows = append(table.Rows, metaV1.TableRow{
+			Cells: []interface{}{
+				object.Kind,
+				object.Name,
+				finalizer,
+			},
+			Object: runtime.RawExtension{Object: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"namespace": object.Namespace,
+					},
+				},
+			}},
+		})
+	}
 }
