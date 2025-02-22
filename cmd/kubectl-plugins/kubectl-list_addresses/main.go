40d06add8a71517421063fb64a7cc08e0980ad81
Gino Latorilla <pblatorilla+github@gmail.com>
2025-02-22 18:29:06 +0800

    Further genericise code of kubectl-list_addresses

--- cmd/kubectl-plugins/kubectl-list_addresses/main.go
+++ cmd/kubectl-plugins/kubectl-list_addresses/main.go
@@ -20,25 +20,19 @@
 package main
 
 import (
-	"fmt"
 	"log/slog"
-	"os"
 
 	devopscmd "github.com/ginolatorilla/devops/cmd/devops/cmd"
 	"github.com/ginolatorilla/devops/pkg/kubectlplugin"
 	"github.com/spf13/cobra"
 
 	coreV1 "k8s.io/api/core/v1"
-	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	gocoreV1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/cli-runtime/pkg/resource"
 )
 
 func main() {
-	if err := newCommand(kubectlplugin.WithDefaultKubeApi()).Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
+	newCommand(kubectlplugin.WithDefaultKubeApi()).Execute()
 }
 
 func newCommand(runnerOpts ...kubectlplugin.RunnerOpts) *cobra.Command {
@@ -52,111 +46,97 @@ func newCommand(runnerOpts ...kubectlplugin.RunnerOpts) *cobra.Command {
 }
 
 func listAddresses(a kubectlplugin.HandlerArgs) (runtime.Object, error) {
-	client := a.KubeApi.CoreV1()
-
 	tableBuilder := kubectlplugin.NewTableBuilder().AdditionalColumns(
 		kubectlplugin.ResourceKindColumn,
 		kubectlplugin.Column{Name: "Type", Description: "The type of address"},
 		kubectlplugin.Column{Name: "Address", Description: "The IP address"},
 	)
 
-	if err := getServiceAddresses(a, client, tableBuilder); err != nil {
-		slog.Warn("failed to list services", "error", err)
-	}
-
-	if err := getPodAddresses(a, client, tableBuilder); err != nil {
-		slog.Warn("failed to list pods", "error", err)
-	}
-
-	if err := getNodeAddresses(a, client, tableBuilder); err != nil {
-		slog.Warn("failed to list nodes", "error", err)
+	for _, d := range []struct {
+		kind        string
+		visitorFunc resource.VisitorFunc
+	}{
+		{"service", getServiceAddresses(tableBuilder)},
+		{"pod", getPodAddresses(tableBuilder)},
+		{"node", getNodeAddresses(tableBuilder)},
+	} {
+		if err := a.ToResourceBuilder().
+			ResourceTypeOrNameArgs(true, d.kind).
+			Do().
+			Visit(d.visitorFunc); err != nil {
+			slog.Warn("failed to list resources", "kind", d.kind, "error", err)
+		}
 	}
 
 	return &tableBuilder.Table, nil
 }
 
-func getServiceAddresses(
-	a kubectlplugin.HandlerArgs,
-	client gocoreV1.CoreV1Interface,
-	tableBuilder *kubectlplugin.TableBuilder,
-) error {
-	services, err := client.Services(a.Namespace).List(a.Cmd.Context(), metaV1.ListOptions{})
-	if err != nil {
-		return err
-	}
-
-	for _, service := range services.Items {
-		for _, ip := range service.Spec.ClusterIPs {
-			if ip == "" || ip == "None" {
-				continue
+func getServiceAddresses(tableBuilder *kubectlplugin.TableBuilder) resource.VisitorFunc {
+	return func(info *resource.Info, err error) error {
+		services := info.Object.(*coreV1.ServiceList)
+		for _, service := range services.Items {
+			for _, ip := range service.Spec.ClusterIPs {
+				if ip == "" || ip == "None" {
+					continue
+				}
+				tableBuilder.AddRow(&service, map[string]any{
+					"Kind":    "Service",
+					"Type":    "ClusterIP",
+					"Address": ip,
+				})
 			}
-			tableBuilder.AddRow(&service, map[string]any{
-				"Kind":    "Service",
-				"Type":    "ClusterIP",
-				"Address": ip,
-			})
-		}
 
-		for _, ip := range service.Spec.ExternalIPs {
-			tableBuilder.AddRow(&service, map[string]any{
-				"Kind":    "Service",
-				"Type":    "ExternalIP",
-				"Address": ip,
-			})
-		}
+			for _, ip := range service.Spec.ExternalIPs {
+				tableBuilder.AddRow(&service, map[string]any{
+					"Kind":    "Service",
+					"Type":    "ExternalIP",
+					"Address": ip,
+				})
+			}
 
-		if service.Spec.LoadBalancerIP != "" {
-			tableBuilder.AddRow(&service, map[string]any{
-				"Kind":    "Service",
-				"Type":    "LoadBalancerIP",
-				"Address": service.Spec.LoadBalancerIP,
-			})
+			if service.Spec.LoadBalancerIP != "" {
+				tableBuilder.AddRow(&service, map[string]any{
+					"Kind":    "Service",
+					"Type":    "LoadBalancerIP",
+					"Address": service.Spec.LoadBalancerIP,
+				})
+			}
 		}
+		return nil
 	}
-	return nil
 }
 
-func getPodAddresses(
-	a kubectlplugin.HandlerArgs,
-	client gocoreV1.CoreV1Interface,
-	tableBuilder *kubectlplugin.TableBuilder,
-) error {
-	pods, err := client.Pods(a.Namespace).List(a.Cmd.Context(), metaV1.ListOptions{})
-	if err != nil {
-		return err
-	}
-	for _, pod := range pods.Items {
-		for _, ip := range pod.Status.PodIPs {
-			tableBuilder.AddRow(&pod, map[string]any{
-				"Kind":    "Pod",
-				"Type":    "PodIP",
-				"Address": ip.IP,
-			})
+func getPodAddresses(tableBuilder *kubectlplugin.TableBuilder) resource.VisitorFunc {
+	return func(info *resource.Info, err error) error {
+		pods := info.Object.(*coreV1.PodList)
+		for _, pod := range pods.Items {
+			for _, ip := range pod.Status.PodIPs {
+				tableBuilder.AddRow(&pod, map[string]any{
+					"Kind":    "Pod",
+					"Type":    "PodIP",
+					"Address": ip.IP,
+				})
+			}
 		}
+		return nil
 	}
-	return nil
 }
 
-func getNodeAddresses(
-	a kubectlplugin.HandlerArgs,
-	client gocoreV1.CoreV1Interface,
-	tableBuilder *kubectlplugin.TableBuilder,
-) error {
-	nodes, err := client.Nodes().List(a.Cmd.Context(), metaV1.ListOptions{})
-	if err != nil {
-		return err
-	}
-	for _, node := range nodes.Items {
-		for _, address := range node.Status.Addresses {
-			if address.Type != coreV1.NodeInternalIP && address.Type != coreV1.NodeExternalIP {
-				continue
+func getNodeAddresses(tableBuilder *kubectlplugin.TableBuilder) resource.VisitorFunc {
+	return func(info *resource.Info, err error) error {
+		nodes := info.Object.(*coreV1.NodeList)
+		for _, node := range nodes.Items {
+			for _, address := range node.Status.Addresses {
+				if address.Type != coreV1.NodeInternalIP && address.Type != coreV1.NodeExternalIP {
+					continue
+				}
+				tableBuilder.AddRow(&node, map[string]any{
+					"Kind":    "Node",
+					"Type":    address.Type,
+					"Address": address.Address,
+				})
 			}
-			tableBuilder.AddRow(&node, map[string]any{
-				"Kind":    "Node",
-				"Type":    address.Type,
-				"Address": address.Address,
-			})
 		}
+		return nil
 	}
-	return nil
 }
