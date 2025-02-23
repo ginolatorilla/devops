fadd595dc48f65b7213552d37842bbd2ebea2f02
Gino Latorilla <pblatorilla+github@gmail.com>
2025-02-24 00:09:30 +0800

    Drop x509 template function

--- cmd/devops/cmd/template.go
+++ cmd/devops/cmd/template.go
@@ -20,19 +20,15 @@
 package cmd
 
 import (
-	"crypto/x509"
-	"encoding/pem"
 	"io"
 	"text/template"
 
 	"github.com/Masterminds/sprig/v3"
 	"github.com/dustin/go-humanize"
-	"github.com/fatih/structs"
 	"github.com/spf13/cobra"
 )
 
 var funcMap = template.FuncMap{
-	"x509":         parseX509ToMap,
 	"relativeTime": humanize.Time,
 }
 
@@ -44,7 +40,6 @@ func newTemplateCmd() *cobra.Command {
 
 The following functions are available in the template:
 - Sprig functions (see http://masterminds.github.io/sprig/)
-- x509: Parse a PEM-encoded X.509 certificate and return a map of its fields (see https://pkg.go.dev/crypto/x509#Certificate)
 - relativeTime: Convert a time.Time to a human-readable relative time`,
 		SilenceUsage: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -69,10 +64,4 @@ func render(stdin io.Reader, stdout io.Writer) error {
 		return err
 	}
 	return nil
-}
-
-func parseX509ToMap(input string) map[string]any {
-	der, _ := pem.Decode([]byte(input))
-	cert := _must(x509.ParseCertificate(der.Bytes))
-	return structs.Map(cert)
 }
--- cmd/devops/cmd/template_test.go
+++ cmd/devops/cmd/template_test.go
@@ -2,8 +2,6 @@ package cmd
 
 import (
 	"bytes"
-	"fmt"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,21 +18,4 @@ func TestTemplate(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, "Hello, World!", out.String())
-}
-
-func TestTemplateParseX509(t *testing.T) {
-	t.Parallel()
-
-	crtRaw, err := os.ReadFile("testdata/tls.crt")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	cmd := newTemplateCmd()
-	cmd.SetIn(bytes.NewBufferString(fmt.Sprintf("{{ index (`%s` | x509) `NotAfter` }}", string(crtRaw))))
-	var out bytes.Buffer
-	cmd.SetOut(&out)
-
-	assert.NoError(t, cmd.Execute())
-	assert.Equal(t, "2026-02-01 06:31:00 +0000 UTC", out.String())
 }
